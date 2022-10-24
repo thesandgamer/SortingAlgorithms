ef65b6cdefb1c1da812feb75fce6373cb3ff9b67
NOURIGAT Adrien <adrien.nourigat@artfx34.lan>
2022-10-24 17:43:36 +0200

    [Upd] fix first step problem

--- SortingAlgorithms/QuickSort.cpp
+++ SortingAlgorithms/QuickSort.cpp
@@ -39,15 +39,17 @@ void QuickSort::QuickSortVector(vector<float>* numbers)
 			
 			numbers->emplace(numbers->begin() + pivotIndex+1 ,numbers->at(i));//Rajoute une case avec la valeur de i après le pivot
 			pivotIndex--;	//Comme on à rajouté un case après, le pivot est plus loin
+			float tempInum = numbers->at(pivotIndex - 1);
+			numbers->at(i) = tempInum;	//La case i prend la valeur de celle avant le pivot
+		    numbers->erase(numbers->begin() + pivotIndex - 1);//On enlève la case i 
+
+
+			/*
 			numbers->erase(numbers->begin() + i);	//On enlève la case où était i 
-
-			
-
 			float tempNum = numbers->at(pivotIndex - 1);	//On va stoquer la valeur de la case avant celle du pivot
-			numbers->erase(numbers->begin() + pivotIndex - 1);	//On va supprimer cette case
-
-			numbers->emplace(numbers->begin() + i, tempNum);
-
+			numbers->erase(numbers->begin() + pivotIndex - 1);	//On va supprimer cette case (celle avant le pivot)
+			numbers->emplace(numbers->begin() + i, tempNum);	//Et on va en créer une après la case supprimé de i
+			*/
 
 		}
 		else
